b860810306d724e6bb13d4ff6c44f664366a3925
Calvin Beck <hobbes@ualberta.ca>
2013-08-19 10:13:50 -0600

    Added the function declarations necessary for the interface.

--- muxduino/mux_channel.h
+++ muxduino/mux_channel.h
@@ -56,4 +56,51 @@ typedef struct MuxChannelList {
     MuxChannelNode *tail;
 } MuxChannelList;
 
+
+/*
+  Arguments:
+      list: The channel list that we want to search.
+
+      channel: The number of the channel we want to find.
+
+  Returns a pointer to the channel node which has the given channel
+  number. If there is so such channel node in the list then NULL is
+  returned.
+
+ */
+
+MuxChannelNode * find_channel_node(MuxChannelList *list, int channel);
+
+
+/*
+  Arguments:
+      list: The channel list we are adding to.
+
+      pipe: The pipe with the channel that we want to add.
+
+  Makes sure that the channel / input of the pipe is in the channel
+  list. This function may allocate memory if the channel / input is
+  not already in the list. If the channel / input pair is already in
+  the list then the function will do nothing -- duplicates are
+  ignored.
+
+ */
+
+void mux_channel_list_add(MuxChannelList *list, MuxPipe pipe);
+
+
+/*
+  Arguments:
+      list: The channel list we are removing from.
+
+      pipe: The pipe with the channel that we want to remove.
+
+  Removes the channel's input if it is in the list and frees the
+  memory. If the channel has no more inputs after it is removed then
+  the channel node will be freed as well.
+
+ */
+
+void mux_channel_list_remove(MuxChannelList *list, MuxPipe pipe);
+
 #endif
